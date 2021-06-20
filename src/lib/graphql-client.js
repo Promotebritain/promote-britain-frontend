64a42a85689bb83be1032af35a27cf4c727180f4
spences10 <spences10apps@gmail.com>
2021-06-20 22:41:43 +0100

    chore: :wrench: add json content type

--- src/lib/graphql-client.js
+++ src/lib/graphql-client.js
@@ -2,5 +2,5 @@ import { GraphQLClient } from 'graphql-request';
 import { API_URL } from './endpoint';
 
 export const strapiClient = new GraphQLClient(API_URL, {
-	headers: {}
+	headers: { 'content-type': 'application/json' }
 });

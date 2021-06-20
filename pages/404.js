630c691b5a584260b92253e14412ed7eb8774fde
spences10 <spences10apps@gmail.com>
2021-06-20 16:19:44 +0100

    feat: :sparkles: init with SvelteKit

--- pages/404.js
+++
@@ -1,9 +0,0 @@
-import { Layout } from '@/components/layout'
-
-export default function NotFound() {
-  return (
-    <Layout>
-      <p>404</p>
-    </Layout>
-  )
-}
--- pages/companies/index.js
+++
@@ -1,9 +0,0 @@
-import { Layout } from '@/components/layout'
-
-export default function CompaniesPage() {
-  return (
-    <Layout>
-      <h1>Companies</h1>
-    </Layout>
-  )
-}

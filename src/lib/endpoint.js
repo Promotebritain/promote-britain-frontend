b31ee96c4b4ca3bedc04ce6e55bc55f07d98d2a2
spences10 <spences10apps@gmail.com>
2021-06-20 21:24:35 +0100

    feat: :sparkles: get basic endpoint working

---
+++ src/lib/endpoint.js
@@ -0,0 +1,2 @@
+export const API_URL = 'http://localhost:1337/graphql';
+// export const API_URL = import.meta.env.VITE_STRAPI_URL || 'http://localhost:1337/graphql';
---
+++ src/routes/countries/index.svelte
@@ -0,0 +1,23 @@
+<script context="module">
+	export async function load({ fetch }) {
+		const res = await fetch('/countries.json');
+		const { countries } = await res.json();
+		return {
+			props: { countries }
+		};
+	}
+</script>
+
+<script>
+	export let countries;
+</script>
+
+{#each countries as { slug, name }}
+	<ul>
+		<li>
+			<a href="/countries/{slug}">
+				{name}
+			</a>
+		</li>
+	</ul>
+{/each}

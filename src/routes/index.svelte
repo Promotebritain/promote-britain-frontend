0c0d4740355a1dfa4b3814359d7fe443b90aca72
spences10 <spences10apps@gmail.com>
2021-06-21 08:49:10 +0100

    feat: :sparkles: add countries to index page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,30 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script context="module">
+	export const hydrate = false;
+
+	export async function load({ fetch }) {
+		const res = await fetch('/countries.json');
+		if (res.ok) {
+			const { countries } = await res.json();
+
+			return {
+				props: { countries }
+			};
+		}
+
+		const { message } = await res.json();
+
+		return {
+			error: new Error(message)
+		};
+	}
+</script>
+
+<script>
+	export let countries;
+</script>
+
+<ul>
+	{#each countries as { id, name }}
+		<li><a href="/countries/{id}">{name}</a></li>
+	{/each}
+</ul>

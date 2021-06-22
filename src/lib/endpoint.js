f4d57fd4c489e2d1fdb7396d576f0949643d817c
spences10 <spences10apps@gmail.com>
2021-06-22 21:56:52 +0100

    feat: :wrench: use env-cmd for secrets

--- src/lib/endpoint.js
+++ src/lib/endpoint.js
@@ -1,2 +1,2 @@
-export const API_URL = 'http://localhost:1337/graphql';
-// export const API_URL = import.meta.env.VITE_STRAPI_URL || 'http://localhost:1337/graphql';
+export const API_URL = process.env['STRAPI_URL']
+// export const API_URL = import.meta.env.STRAPI_URL || 'http://localhost:1337/graphql';

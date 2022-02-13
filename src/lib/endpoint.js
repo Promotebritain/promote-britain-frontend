c0aadb3a224900ce527e596e37aa8463707b2855
spences10 <spences10apps@gmail.com>
2022-02-13 13:29:39 +0000

    refactor: :fire: start over with new config

--- src/lib/endpoint.js
+++
@@ -1,2 +0,0 @@
-export const API_URL = process.env['STRAPI_URL']
-// export const API_URL = import.meta.env.STRAPI_URL || 'http://localhost:1337/graphql';
--- src/lib/graphql-client.js
+++
@@ -1,6 +0,0 @@
-import { GraphQLClient } from 'graphql-request'
-import { API_URL } from './endpoint'
-
-export const strapiClient = new GraphQLClient(API_URL, {
-  headers: { 'content-type': 'application/json' },
-})
